c16ee7a022a9e806377d58bc5782545bda3d8091
Sahil76nam <namdevsahil6@gmail.com>
2024-11-23 19:43:46 +0530

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+// function run()
+// {
+// redeclaration not allowed in let and counst exept var
+// const num=44;
+// const num=55;
+// console.log(num)
+
+//============reassignment=========================
+// var num=55;
+// num=88;
+// console.log(num);
+// let num1=55;
+// num1=88;
+// console.log(num1);
+function run(){
+    alert("hello")
+}
+
+
+// run();
---
+++ vote.js
@@ -0,0 +1,64 @@
+
+function run(){
+
+
+    // let num=prompt("Enter you age");
+    // (num>=18)?alert("eligble for vote"):alert("not eligble")
+    // let num1=confirm(" have you completed a course");
+    // let num2=confirm("have you got certificate");
+    // (num1&&num2)?alert("comfirm and ok"):alert("not completed and alert")
+
+    // if(num1 && num2){
+    //     alert("comfirm and ok")
+    
+    // }
+    // else{
+    //     alert("not completed and alert")
+    // }
+    //  let num1=prompt("enter the number");
+    //  if(num1%2==0){
+    //     alert("even")
+    //  }
+    //  else{
+    //     alert("odd")
+    //  }
+    // let num1=prompt("enter the price");
+    // let num2=confirm("they have discount coupon");
+    // if("ok"){
+    //     alert("price ="+(num1-(.1*num1)))
+    // }
+    // else{ 
+    //     alert(num1)
+        //}
+    // let number=prompt("enter exam store number");
+    // if(number>=33){
+    //     alert("user passed in exam")
+    // }
+    // else{
+    //     alert("failed in exam ")
+    // }
+    // let conformation=confirm("are you sure to delete message");
+    // if(conformation){
+    //     alert("item deleted")
+    // }
+    // else {
+    //     alert("cancelled")
+    // }
+    //   let number=prompt("enter number");
+    //   while(number<=10)
+    //   {
+    //     alert(number)
+    //     number++;
+    //   }
+      let paar=document.getElementById("text");
+
+      paar.textContent="sahil amdev";
+      paar.style.color="red";
+      paar.style.fontSize="50px";
+      
+
+      
+ 
+    }
+
+
