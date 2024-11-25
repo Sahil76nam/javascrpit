4b905923d54b17329b44df46eb4b217ddfc490bf
Sahil76nam <namdevsahil6@gmail.com>
2024-11-25 19:57:26 +0530

    update

--- arrays.js
+++ arrays.js
@@ -1,10 +1,12 @@
 function arr1(){
 
     let fruits=["apple","mango","banana"]
-    let ans=document.querySelector("#fruits")
+    // let ans=document.querySelector("#fruits")
 
-    alert(fruits)
-    ans.innerHTML=fruits[0];
+    // alert(fruits)
+    // ans.innerHTML=fruits[0];
+    fruits.push("grapis");
+    console.log(fruits)
 
 
 }
