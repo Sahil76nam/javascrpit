692bf39b01159ac01632d9bbeebed6d86454a8e0
Sahil76nam <namdevsahil6@gmail.com>
2024-11-27 18:51:43 +0530

    update

--- string.js
+++ string.js
@@ -1,6 +1,22 @@
 const letsgo=()=>{
 
-let text="this a sring";
+let text="   this a sring This   ";
+let text1="this is big string"
+
+
+console.log(text.length);//17
+console.log(text.slice(2,6));//is a
+console.log(text.concat(" ",text1));//this a sring This this is big string
+console.log(text.toUpperCase());//THIS A SRING THIS
+console.log(text.substring(1,8));//his a s
+console.log(text.replace("this","hello"));//hello a sring This
+console.log(text.replace(/this/g,"hello"));//hello a sring This
+console.log(text.replace(/this/gi,"hello"));//hello a sring hello
+console.log(text.trim());//first space and end sapce remove ho jata h
+
+
+
+
 
 }
 letsgo()
