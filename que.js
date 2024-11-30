bcb492d73215ebb5e94db7ce5bf76845fc0f49c0
Sahil76nam <namdevsahil6@gmail.com>
2024-11-30 19:41:41 +0530

    update

--- que.js
+++ que.js
@@ -4,5 +4,21 @@ let square=(a)=>{
     let pera=document.querySelector("#pera")
 
     // pera.innerHTML=a*a;
-    pera.innerHTML=`the square of ${a} is ${a*a}`;
+    pera.innerHTML=`
+    the square of ${a} is ${a*a} <br>
+
+
+    cube is ${a} is ${a*a*a} <br>
+
+    ${(a%2==0)?even:odd}
+    
+        
+    
+
+    
+    
+    
+    
+    `;
+    
 }
