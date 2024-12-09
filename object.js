35b67d4b2e2d551401489ca7ab58b3f264f62230
Sahil76nam <namdevsahil6@gmail.com>
2024-12-09 20:14:27 +0530

    update

--- object.js
+++ object.js
@@ -4,21 +4,35 @@
 //     key1:"value1",
 //     key2:"value2"
 // }
-let student={
-    name:"sahil",
-    age: 20,
-    address:"sainkheda",
-    welcome:function(){
-        return`hello my name is  ${this.name}  and age is ${this.age}`//function mai "this" ka use
-    }
+// let student={
+//     name:"sahil",
+//     age: 20,
+//     address:"sainkheda",
+//     welcome:function(){
+//         return`hello my name is  ${this.name}  and age is ${this.age}`//function mai "this" ka use
+//     }
+// }
+// //for(let "key value" in "objet name")
+// for(let addd in student)
+// {
+//     console.log(student[addd])
+// }
+// console.log(student.name);//access
+// student.gender="male";//added
+// console.log(student);
+// student.age=21;//update
+// console.log(student.welcome());
+
+
+const st=()=>{
+    let student ={name :"sahil",age:22,city:"bhopal"}
+    const { name,age,city}=student;//destructre tree
+    console.log(age)
+
+    student1={name:"sahil",age:23};
+    student2={city:"bhoapl",country:"india"}
+
+    let twostudent={...student1,...student2};//spread opeater
+    console.log(twostudent);
 }
-//for(let "key value" in "objet name")
-for(let addd in student)
-{
-    console.log(student[addd])
-}
-console.log(student.name);//access
-student.gender="male";//added
-console.log(student);
-student.age=21;//update
-console.log(student.welcome());
+st();
