3381750e34fdb02dac7153bd3bb5678337bfc0bf
Sahil76nam <namdevsahil6@gmail.com>
2024-12-10 14:03:58 +0530

    update

--- object.js
+++ object.js
@@ -25,14 +25,22 @@
 
 
 const st=()=>{
-    let student ={name :"sahil",age:22,city:"bhopal"}
-    const { name,age,city}=student;//destructre tree
-    console.log(age)
+//     let student ={name :"sahil",age:22,city:"bhopal"}
+//     const { name,age,city}=student;//destructre tree
+//     console.log(age)
 
-    student1={name:"sahil",age:23};
-    student2={city:"bhoapl",country:"india"}
+//     student1={name:"sahil",age:23};
+//     student2={city:"bhoapl",country:"india"}
 
-    let twostudent={...student1,...student2};//spread opeater
-    console.log(twostudent);
+//     let twostudent={...student1,...student2};//spread opeater
+//     console.log(twostudent);
+// }
+
+
+
+let arr1=[1,3,4,5,67,7];
+let newarr=arr1.filter((num)=>num>3)//filter function
+//let newarr=arr1.map((num)=>num*2);//map function
+console.log(newarr)
 }
 st();
